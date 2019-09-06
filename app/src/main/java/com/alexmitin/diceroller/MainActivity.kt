caae9afdfb1ffa90a98312dd45109674084876e5
Alexandr <alex.mitin.psmu@gmail.com>
2019-09-06 14:20:52 +0500

    1.21

--- app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
+++ app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.alexmitin.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -21,8 +22,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val dice: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableRes = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice.setImageResource(drawableRes)
     }
 }
