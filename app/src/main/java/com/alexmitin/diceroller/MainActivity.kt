1da4c71a6e1f91d253a30e75ef54e50e9bdeb7c1
Alexandr <alex.mitin.psmu@gmail.com>
2019-09-06 15:05:11 +0500

    1.23 Optimize code by reduce findById calls

--- app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
+++ app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollBtn: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollBtn.text = "Lets Roll!"
         rollBtn.setOnClickListener {
             Toast.makeText(this, "Кубик брошен!", Toast.LENGTH_SHORT).show()
@@ -22,7 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
         val drawableRes = when (randomInt) {
             1 -> R.drawable.dice_1
@@ -32,6 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        dice.setImageResource(drawableRes)
+        diceImage.setImageResource(drawableRes)
     }
 }
