3cdb05b2b457fd533f97e691021a7574eb4f75b4
Alexandr <alex.mitin.psmu@gmail.com>
2019-09-06 13:40:45 +0500

    1.18

--- app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
+++ app/src/main/java/com/alexmitin/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.alexmitin.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         val rollBtn: Button = findViewById(R.id.roll_button)
         rollBtn.text = "Lets Roll!"
         rollBtn.setOnClickListener {
-            Toast.makeText(this, "Cho Kawo, Suchara!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Кубик брошен!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
